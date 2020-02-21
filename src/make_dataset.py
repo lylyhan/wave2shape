85ff77ff68486b7d400a798f2a93daa87d16d1af
Lily Han <lilyhan0602@gmail.com>
2020-02-21 00:59:36 -0500

    sa

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -6,13 +6,49 @@ import soundfile as sf
 import os
 #from kymatio import Scattering1D
 
-df_train = pandas.read_csv("train_param.csv") 
+df_train = pandas.read_csv("../notebooks/train_param.csv") 
+df_val = pandas.read_csv("../notebooks/val_param.csv") 
+df_test = pandas.read_csv("../notebooks/test_param.csv") 
+
+val_params = df_val.values
+train_params = df_train.values
+test_params = df_test.values
+
+n_samp_val,n_param_val = val_params.shape
+n_samp_test,n_param_val = test_params.shape
+n_samp_train,n_param_val = train_params.shape
+
 
 mode = 10
 sr = 44100
-train_params = df_train.values
-n_samp,n_param = params.shape
-for i in range(n_samp):
+
+for i in range(n_samp_val):
+    omega,tau,p,D,alpha = val_params[i,1:-1]
+    y = ftm.getsounds_imp(mode,mode,omega,tau,p,D,alpha,sr)
+    y = y/ max(y)
+    path_out = "/scratch/hh2263/drum_data/val/"
+    if not os.path.exists(path_out):
+      os.mkdir(path_out)
+    filename = os.path.join(path_out,str(val_params[i,0])+"_sound.wav")
+    sf.write(filename, y, sr)
+
+
+print("finished validation set!!")
+
+for i in range(n_samp_test):
+    omega,tau,p,D,alpha = test_params[i,1:-1]
+    y = ftm.getsounds_imp(mode,mode,omega,tau,p,D,alpha,sr)
+    y = y/ max(y)
+    path_out = "/scratch/hh2263/drum_data/test/"
+    if not os.path.exists(path_out):
+      os.mkdir(path_out)
+    filename = os.path.join(path_out,str(test_params[i,0])+"_sound.wav")
+    sf.write(filename, y, sr)
+    
+    
+print("finished test set!!")
+
+for i in range(n_samp_train):
     omega,tau,p,D,alpha = train_params[i,1:-1]
     y = ftm.getsounds_imp(mode,mode,omega,tau,p,D,alpha,sr)
     y = y/ max(y)
@@ -23,34 +59,7 @@ for i in range(n_samp):
     sf.write(filename, y, sr)
     
 
-
-
-df_test = pandas.read_csv("test_param.csv") 
-test_params = df_test.values
-
-for i in range(n_samp):
-    omega,tau,p,D,alpha = test_params[i,1:-1]
-    y = ftm.getsounds_imp(mode,mode,omega,tau,p,D,alpha,sr)
-    y = y/ max(y)
-    path_out = "/scratch/hh2263/drum_data/test/"
-   	if not os.path.exists(path_out):
-   		os.mkdir(path_out)
-    filename = os.path.join(path_out,str(test_params[i,0])+"_sound.wav")
-    sf.write(filename, y, sr)
-    
-
-df_val = pandas.read_csv("val_param.csv") 
-val_params = df_val.values
-
-for i in range(n_samp):
-    omega,tau,p,D,alpha = val_params[i,1:-1]
-    y = ftm.getsounds_imp(mode,mode,omega,tau,p,D,alpha,sr)
-    y = y/ max(y)
-    path_out = "/scratch/hh2263/drum_data/val/"
-   	if not os.path.exists(path_out):
-   		os.mkdir(path_out)
-    filename = os.path.join(path_out,str(val_params[i,0])+"_sound.wav")
-    sf.write(filename, y, sr)
+print("finished train set!!")
 
 
 
